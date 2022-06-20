9b1bae6e61cd8ee890d39dec61324509b37b51ae
lalm84 <lalm84@live.com>
2022-06-20 16:31:21 -0300

    Root rearrange
    .

---
+++ miapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from miapp.models import BlogModel
+# Register your models here.
+
+
+#admin.site.register(blogs)
+
+admin.site.register(index)
+
+#admin.site.register(Nosotros)
+
+#admin.site.register(Register)
