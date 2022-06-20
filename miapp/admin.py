ead1a93529740e0317b76845c81543879f924555
lolasalvio <96928955+lolasalvio@users.noreply.github.com>
2022-06-20 17:59:34 -0300

    Add files via upload

---
+++ miapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from blog.views import nosotros
+from miapp.models import BlogModel
+from miapp.views import blogs, index, register, nosotros
+# Register your models here.
+
+
+#admin.site.register(blogs)
+
+#admin.site.register(index)
+
+#admin.site.register(nosotros)
+
+#admin.site.register(register)
